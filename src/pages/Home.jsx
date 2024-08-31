c5a57cd0e66118c049ecfd7dcab435a0f6fa40fd
lonesomejoker <shakestha@gmail.com>
2024-08-31 11:50:02 +0545

    Final Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,12 @@ const Home = () => {
   return (
     <div className='container font-poppins font-[500] text-[1rem] '>
       <div className=' flex'>
-      <img src="https://marketplace.canva.com/EAFG6srv-fI/1/0/1131w/canva-white-and-red-clean-minimalist-fashion-magazine-cover-GYbcb8zmyTU.jpg" alt="desc" className=' h-[30rem] w-[19rem] lg:block hidden rounded-l-[2rem]'/>
+      <img src="https://marketplace.canva.com/EAFG6srv-fI/1/0/1131w/canva-white-and-red-clean-minimalist-fashion-magazine-cover-GYbcb8zmyTU.jpg" alt="desc" className=' h-[30rem] w-[19rem] lg:block hidden rounded-bl-[2rem]'/>
       <ProductReusable data={womenItems}/>
     </div>
     <div className=' flex mt-10'>
      <ProductReusable data={menItems}/>
-    <img src="https://th.bing.com/th/id/OIP.B9y2WsVb4oYfCrAjyjl7IwAAAA?rs=1&pid=ImgDetMain" alt="desc" className=' h-[30rem] w-[19rem] lg:block hidden rounded-r-[2rem]'/>
+    <img src="https://th.bing.com/th/id/OIP.B9y2WsVb4oYfCrAjyjl7IwAAAA?rs=1&pid=ImgDetMain" alt="desc" className=' h-[30rem] w-[19rem] lg:block hidden rounded-tr-[2rem]'/>
     </div>
     </div>
   )
